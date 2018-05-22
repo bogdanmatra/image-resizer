a12f36a5428f01a265b504407188f0c7189edf05
Bogdan Matra <matragoc@adobe.com>
2018-05-22 18:21:06 +0300

    Added Spring Boot

---
+++ src/main/java/com/byk/Example.java
@@ -0,0 +1,20 @@
+package com.byk;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
